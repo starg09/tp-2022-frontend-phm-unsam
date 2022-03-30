293d80d42c04eda19bbd8a6651967b0e55b02e9d
Guillermo Bianchi <ultraguille2006@gmail.com>
2022-03-30 16:17:04 -0300

    Agregado footer

--- components/footer.js
+++ components/footer.js
@@ -1,7 +1,14 @@
+import { Container, Navbar } from "react-bootstrap";
+import { BsShop } from "react-icons/bs";
+
 export default function Footer() {
-    return (
-      <>
-        
-      </>
-    )
-  }
+  return (
+    <>
+      <Navbar bg="primary" variant="dark">
+        <Container className="justify-content-center rb-center">
+          <Navbar.Brand><BsShop/> Difficult / 2022</Navbar.Brand>
+        </Container>
+      </Navbar>
+    </>
+  );
+}
