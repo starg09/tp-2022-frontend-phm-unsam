72fcf7baf210131c9c6f8b89ae84610b2a7c40b2
Guillermo Bianchi <ultraguille2006@gmail.com>
2022-03-31 02:45:53 -0300

    Index: Retoques visuales, agregada barra de busqueda

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import { SearchIcon } from "@chakra-ui/icons";
 import {
   Box,
   Button,
@@ -6,6 +7,9 @@ import {
   Heading,
   HStack,
   Image,
+  Input,
+  InputGroup,
+  InputRightElement,
   Radio,
   RadioGroup,
   SimpleGrid,
@@ -30,7 +34,7 @@ export default function Home() {
       puntaje: 2,
       precio: 2536.55,
       origen: "Argentina",
-      descripcion: "Porcelanato rústico marca Acme semi satinado 36 x 36"
+      descripcion: "Porcelanato rústico marca Acme semi satinado 36 x 36",
     },
     {
       id: 2,
@@ -39,7 +43,7 @@ export default function Home() {
       puntaje: 3,
       precio: 1987.37,
       origen: "Argentina",
-      descripcion: "Porcelanato arena marca Acme semi satinado 36 x 36"
+      descripcion: "Porcelanato arena marca Acme semi satinado 36 x 36",
     },
     {
       id: 3,
@@ -48,7 +52,7 @@ export default function Home() {
       puntaje: 1,
       precio: 2996.99,
       origen: "Argentina",
-      descripcion: "Porcelanato beteado marca Acme satinado 36 x 36"
+      descripcion: "Porcelanato beteado marca Acme satinado 36 x 36",
     },
     {
       id: 4,
@@ -57,7 +61,7 @@ export default function Home() {
       puntaje: 4,
       precio: 8836.77,
       origen: "Argentina",
-      descripcion: "Pintura para interiores color rojo marca Adla x 20 litros"
+      descripcion: "Pintura para interiores color rojo marca Adla x 20 litros",
     },
     {
       id: 5,
@@ -66,99 +70,141 @@ export default function Home() {
       puntaje: 4,
       precio: 2536.55,
       origen: "Argentina",
-      descripcion: "Porcelanato rústico oscuro marca Acme semi satinado 36 x 36"
+      descripcion:
+        "Porcelanato rústico oscuro marca Acme semi satinado 36 x 36",
     },
-  ]
+  ];
 
   return (
-    <HStack minH="80vh" alignItems="stretch" bg="orange.200">
-      <Box w="20%" p={3}>
-        <VStack
-          p={4}
-          alignContent="flex-start"
-          alignItems="stretch"
-          bg="purple.300"
-          borderRadius="2vh"
-        >
-          <Heading textAlign="center">Filtros</Heading>
-          <Divider />
-          <RadioGroup onChange={setPuntaje} value={puntaje} colorScheme="purple">
-            <VStack alignItems="flex-start">
-              <Heading size="md" alignSelf="center">
-                Puntaje
-              </Heading>
-              <Radio size="md" value="5">
-                5 puntos
-              </Radio>
-              <Radio size="md" value="4">
-                4 o más puntos
-              </Radio>
-              <Radio size="md" value="3">
-                3 o más puntos
-              </Radio>
-              <Radio size="md" value="2">
-                2 o más puntos
-              </Radio>
-              <Radio size="md" value="0">
-                Todos
-              </Radio>
-            </VStack>
-          </RadioGroup>
-          <Divider />
-          <Heading size="md" alignSelf="center">
-            Origen
-          </Heading>
-          <Checkbox
-            colorScheme="purple"
-            isChecked={todosOrigenes}
-            isIndeterminate={algunosOrigenes}
-            onChange={(e) =>
-              setCheckedItems([e.target.checked, e.target.checked])
-            }
+    <VStack minH="80vh" bg="orange.200">
+      <InputGroup w="60%" my={3} size="md" bg="yellow.100" borderRadius="1.5vw">
+        <Input
+          pr="4.5rem"
+          type="text"
+          placeholder="Buscar" borderRadius="1.5vw"
+        />
+        <InputRightElement width="4.5rem">
+          <Button h="1.75rem" size="sm" colorScheme="blue" borderRadius="1.5vw">
+            <SearchIcon/>
+          </Button>
+        </InputRightElement>
+      </InputGroup>
+      <HStack alignItems="stretch">
+        <Box w="20%" p={3}>
+          <VStack
+            p={4}
+            alignContent="flex-start"
+            alignItems="stretch"
+            bg="purple.300"
+            borderRadius="2vh"
           >
-            Todos
-          </Checkbox>
-          <VStack alignItems="flex-start" pl={6} mt={1} spacing={1}>
+            <Heading textAlign="center">Filtros</Heading>
+            <Divider />
+            <RadioGroup
+              onChange={setPuntaje}
+              value={puntaje}
+              colorScheme="purple"
+            >
+              <VStack alignItems="flex-start">
+                <Heading size="md" alignSelf="center">
+                  Puntaje
+                </Heading>
+                <Radio size="md" value="5">
+                  5 puntos
+                </Radio>
+                <Radio size="md" value="4">
+                  4 o más puntos
+                </Radio>
+                <Radio size="md" value="3">
+                  3 o más puntos
+                </Radio>
+                <Radio size="md" value="2">
+                  2 o más puntos
+                </Radio>
+                <Radio size="md" value="0">
+                  Todos
+                </Radio>
+              </VStack>
+            </RadioGroup>
+            <Divider />
+            <Heading size="md" alignSelf="center">
+              Origen
+            </Heading>
             <Checkbox
               colorScheme="purple"
-              isChecked={checkedItems[0]}
+              isChecked={todosOrigenes}
+              isIndeterminate={algunosOrigenes}
               onChange={(e) =>
-                setCheckedItems([e.target.checked, checkedItems[1]])
+                setCheckedItems([e.target.checked, e.target.checked])
               }
             >
-              Argentina
+              Todos
             </Checkbox>
-            <Checkbox
-              colorScheme="purple"
-              isChecked={checkedItems[1]}
-              onChange={(e) =>
-                setCheckedItems([checkedItems[0], e.target.checked])
-              }
-            >
-              Brasil
-            </Checkbox>
-          </VStack>
-        </VStack>
-      </Box>
-      <SimpleGrid columns={4} w="80%" maxH="100%" overflowY="auto" p={3}>
-        {itemsTemp.map( i => (
-          <Box minH="40vh" size="full" m={5} borderRadius="2vh" bg="green.400">
-            <VStack my={5} alignItems="flex-start">
-              <Image alignSelf="center" w="60%" h="auto" src={i.imagen ? i.imagen : "img_placeholder.png"}/>
-              <Divider py={1} />
-              <HStack px={4} fontSize="sm" w="full" justifyContent="space-between">
-                <Text>{i.nombre}</Text>
-                <Text>{i.puntaje}*</Text>
-              </HStack>
-              <Heading alignSelf="center" size="lg">${i.precio}</Heading>
-              <Text px={4} fontSize="xs">Origen: {i.origen ? i.origen : "Desconocido"}</Text>
-              <Text px={4} fontSize="sm" fontStyle="italic">{i.descripcion}</Text>
-              <Button alignSelf="center" colorScheme="purple">Agregar al carrito</Button>
+            <VStack alignItems="flex-start" pl={6} mt={1} spacing={1}>
+              <Checkbox
+                colorScheme="purple"
+                isChecked={checkedItems[0]}
+                onChange={(e) =>
+                  setCheckedItems([e.target.checked, checkedItems[1]])
+                }
+              >
+                Argentina
+              </Checkbox>
+              <Checkbox
+                colorScheme="purple"
+                isChecked={checkedItems[1]}
+                onChange={(e) =>
+                  setCheckedItems([checkedItems[0], e.target.checked])
+                }
+              >
+                Brasil
+              </Checkbox>
             </VStack>
-          </Box>
-        )
-        )}
-      </SimpleGrid>
-    </HStack>
+          </VStack>
+        </Box>
+        <SimpleGrid columns={4} w="80%" maxH="100%" overflowY="auto" p={3}>
+          {itemsTemp.map((i) => (
+            <Box
+              minH="40vh"
+              size="full"
+              m={5}
+              borderRadius="2vh"
+              bg="green.400"
+            >
+              <VStack my={5} alignItems="flex-start">
+                <Image
+                  alignSelf="center"
+                  w="60%"
+                  h="auto"
+                  src={i.imagen ? i.imagen : "img_placeholder.png"}
+                />
+                <Divider py={1} />
+                <HStack
+                  px={4}
+                  fontSize="sm"
+                  w="full"
+                  justifyContent="space-between"
+                >
+                  <Text>{i.nombre}</Text>
+                  <Text>{i.puntaje}*</Text>
+                </HStack>
+                <Heading alignSelf="center" size="lg">
+                  ${i.precio}
+                </Heading>
+                <Text px={4} fontSize="xs">
+                  Origen: {i.origen ? i.origen : "Desconocido"}
+                </Text>
+                <Text px={4} fontSize="sm" fontStyle="italic">
+                  {i.descripcion}
+                </Text>
+                <Button alignSelf="center" colorScheme="purple">
+                  Agregar al carrito
+                </Button>
+              </VStack>
+            </Box>
+          ))}
+        </SimpleGrid>
+      </HStack>
+    </VStack>
   );
 }
