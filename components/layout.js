03ee1fe3f5df9af994f5ff60633ec2eea1855314
porwaltz <38365041+porwaltz@users.noreply.github.com>
2022-03-31 05:58:09 -0300

    carrito agregado

--- components/layout.js
+++ components/layout.js
@@ -3,12 +3,14 @@ import Footer from './footer'
 import ComprasRealizadasr from './comprasRealizadas'
 import Perfil from './perfil'
 import PerfilDeUsuario from '../pages/perfilDeUsuario'
+import Carrito from './carrito'
 
 export default function Layout({ children }) {
   return (
     <>
       <Navbar />
-      <PerfilDeUsuario />
+      <Carrito />
+      {/*<PerfilDeUsuario />*/}
       {/*<main>{children}</main>*/}
       <Footer />
     </>
