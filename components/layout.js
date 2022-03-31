d3a0f3c6d8b0a662ca5c0d3536c70bd6585fcda6
porwaltz <38365041+porwaltz@users.noreply.github.com>
2022-03-31 13:51:56 -0300

    sticky footer

--- components/layout.js
+++ components/layout.js
@@ -4,15 +4,19 @@ import ComprasRealizadasr from './comprasRealizadas'
 import Perfil from './perfil'
 import PerfilDeUsuario from '../pages/perfilDeUsuario'
 import Carrito from './carrito'
+import { Flex, Spacer } from '@chakra-ui/react'
 
 export default function Layout({ children }) {
   return (
-    <>
+    <Flex
+    direction='column'
+    minH='100vh'>
       <Navbar />
       <Carrito />
+      <Spacer />
       {/*<PerfilDeUsuario />*/}
       {/*<main>{children}</main>*/}
       <Footer />
-    </>
+    </Flex>
   )
 }
