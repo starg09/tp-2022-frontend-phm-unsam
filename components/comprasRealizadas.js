2789eaf0208a8a2b2f55e132a3de78db3aaa8430
Guillermo Bianchi <ultraguille2006@gmail.com>
2022-05-13 19:19:18 -0300

    Arreglos a comprasRealizadas

--- components/comprasRealizadas.js
+++ components/comprasRealizadas.js
@@ -1,13 +1,12 @@
-import { Heading } from '@chakra-ui/react'
-import { useState, useEffect } from 'react'
-import { usuariosService } from '../services/usuario.service'
+import { Heading } from "@chakra-ui/react";
+import { useState, useEffect } from "react";
+import { usuariosService } from "../services/usuario.service";
 
 export default function ComprasRealizadas(props) {
-    //traer las compras en una lista
-    const [compras, set_compras] = useState([])
-    const [renderCompras, set_renderCompras] = useState([])
-    let [loaded, set_loaded] = useState(false)
-    /* const compras = [
+  //traer las compras en una lista
+  const [compras, set_compras] = useState([]);
+  let [loaded, set_loaded] = useState(false);
+  /* const compras = [
         {
             orden: "#1",
             fecha: "01/02/2022",
@@ -22,28 +21,19 @@ export default function ComprasRealizadas(props) {
         }
     ] */
 
-    async function arrancar(){
-        const llamado = await usuariosService.comprasUsuario(props.userId)
-        set_compras([...llamado])
-        const renderComprasComponent = compras.map( c => 
-            <tr>
-            <th scope="row">{c.ordenCompra}</th>
-            <td>{c.fechaCompra}</td>
-            <td>{c.cantidad}</td>
-            <td>{c.importe}</td>
-            </tr>
-            )
-        set_renderCompras([...renderComprasComponent])
+  async function arrancar() {
+    const llamado = await usuariosService.comprasUsuario(props.userId);
+    set_compras([...llamado]);
+  }
+
+  useEffect(async () => {
+    await arrancar();
+    if (!loaded) {
+      set_loaded(true);
     }
-    
-    useEffect( async() => { 
-        if(!loaded){
-            await arrancar()
-            set_loaded(true)
-            } 
-        })
-    
-    /* const renderCompras = compras.map( c => 
+  }, []);
+
+  /* const renderCompras = compras.map( c =>
         <tr>
         <th scope="row">{c.orden}</th>
         <td>{c.fecha}</td>
@@ -51,23 +41,29 @@ export default function ComprasRealizadas(props) {
         <td>{c.importe}</td>
         </tr>
         ) */
-    return (
-        <div className="w-75"> 
-            <Heading size={"md"}>Compras</Heading>
-            <table className="table table-striped">
-                <thead>
-                    <tr>
-                    <th scope="col">Orden de compra</th>
-                    <th scope="col">Fecha</th>
-                    <th scope="col">Cantidad de articulos</th>
-                    <th scope="col">Importe total</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {renderCompras}
-                </tbody>
-            </table>
-        </div>
-
-    )
+  return (
+    <div className="w-75">
+      <Heading size={"md"}>Compras</Heading>
+      <table className="table table-striped">
+        <thead>
+          <tr>
+            <th scope="col">Orden de compra</th>
+            <th scope="col">Fecha</th>
+            <th scope="col">Cantidad de articulos</th>
+            <th scope="col">Importe total</th>
+          </tr>
+        </thead>
+        <tbody>{compras.map(
+            (c) => (
+                <tr>
+                    <th scope="row">{c.ordenCompra}</th>
+                    <td>{c.fechaCompra}</td>
+                    <td>{c.cantidad}</td>
+                    <td>{c.importe}</td>
+                </tr>
+            )
+    )}</tbody>
+      </table>
+    </div>
+  );
 }
