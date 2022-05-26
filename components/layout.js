247371741d990f950471e12ef5d7e6daccd321e6
Guillermo Bianchi <ultraguille2006@gmail.com>
2022-05-26 02:57:35 -0300

    Remover archivo no utilizado

--- components/layout.js
+++ components/layout.js
@@ -1,12 +1,6 @@
 import Navbar from './navbar'
 import Footer from './footer'
-import ComprasRealizadasr from './comprasRealizadas'
-import Perfil from './perfil'
-import PerfilDeUsuario from '../pages/perfilDeUsuario'
-import Carrito from './carrito'
 import { Flex, Spacer } from '@chakra-ui/react'
-import TablaProducto from './tablaProducto'
-import Producto from './producto'
 import { useRouter } from 'next/router'
 
 
